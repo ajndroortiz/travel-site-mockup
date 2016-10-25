e01b18913461fb9cda06c6d2c0b6af96955f09ff
Alex <zitroxelamedia@gmail.com>
2016-10-25 14:23:59 -0400

    Added comments to provide documentation

--- gulp/tasks/sprite.js
+++ gulp/tasks/sprite.js
@@ -4,6 +4,7 @@ rename = require('gulp-rename'),
 del = require('del'),
 svg2png = require('gulp-svg2png');
 
+// Configuration for gulp-svg-sprite
 var config = {
   shape: {
     spacing: {
@@ -29,35 +30,42 @@ var config = {
   }
 };
 
+// Delete Sprite folder to ensure always working with new version
 gulp.task('beginClean', function() {
   return del(['./app/temp/sprite', './app/assets/images/sprites']);
 });
 
+// Task to create Sprites
 gulp.task('createSprite', ['beginClean'], function() {
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
+// Task to convert SVG's to PNG's
 gulp.task('createpngcopy', ['createSprite'], function() {
   return gulp.src('./app/temp/sprite/css/*.svg')
   .pipe(svg2png())
   .pipe(gulp.dest('./app/temp/sprite/css'));
 });
 
+// Copy Sprites and PNGs to destination folders
 gulp.task('copySpriteGraphic', ['createpngcopy'], function() {
   return gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
     .pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
+// Copy sprite.css file
 gulp.task('copySpriteCSS', ['createSprite'], function() {
   return gulp.src('./app/temp/sprite/css/*.css')
     .pipe(rename('_sprite.css'))
     .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
+// End of the Clean process which delets the Sprite folder
 gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
   return del('./app/temp/sprite');
 });
 
+// Default Icon task
 gulp.task('icons', ['beginClean', 'createSprite', 'createpngcopy', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
